dfca8d61c7e32d5f84c49ec5178fbaec74e32356
Gonzalo.Manjaro <fxgxcx@hotmail.com>
2018-11-13 10:53:00 +0100

    Makefile

--- src/GameObject.h
+++ src/GameObject.h
@@ -12,15 +12,27 @@ public:
 
   GameObject(){}
 
-  GameObject(int x, int y, int velocity){
+  GameObject(int x, int y){
     this->x = x;
     this->y = y;
-    this->velocity = velocity;
   }
 
   void addComponent(Component* c){
     components.push_back(c);
   }
+
+  //Get Component
+  template<typename T>
+  T* getComponent() {
+      for(int i=0; i<components.size(); i++){
+          if(T* c = static_cast<T*>(components[i])){
+              return c;
+          }
+      }
+      
+      return nullptr;
+  }   
+
 private:
   std::vector<Component*> components;
 };
