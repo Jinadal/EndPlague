a19f5c5990ca09484dbf8491ed0285b445527ca3
Jinadal <jinadalsanchez@gmail.com>
2018-12-02 17:09:27 +0100

    comentado

--- Game/src/GameFacade/World.cpp
+++ Game/src/GameFacade/World.cpp
@@ -8,15 +8,21 @@ void World::initWorldPhysics(){
     //btDbvtBroadphase = uses a fast dynamic bounding volume hierarchy based on AABB tree
     //Collision has 2 phases: 1º broad that discards all objects that cannot collide and 
     //2º narrow that creates a list with all the objects that can collide (this method is 
-    //slower). 
+    //slower). DbvtBroadphase is specially used for AABB tree implementation.
     _broadphase = new btDbvtBroadphase();
 
+
+    //Responsable of the collision management and detection. Contains default setup for memory, collision setup.
     _collisionConfiguration = new btDefaultCollisionConfiguration();
     _dispatcher = new btCollisionDispatcher(_collisionConfiguration);
 
+    //Responsable of the correct interaction, taking into account gravity,
+    //game logic supplied forces, collisions, and hinge constraints.
     _solver = new btSequentialImpulseConstraintSolver();
 
+    //Creation of the world and input the configurations
     _world = new btDiscreteDynamicsWorld(_dispatcher, _broadphase, _solver, _collisionConfiguration);
 
+    //Set gravity to physics in y=-9,8
     _world->setGravity(btVector3(0,-9.8,0));
 }
