86ff1313a64b7f3539f6e6ed93457f605ae4665a
Gonzalo G Campos <fxgxcx@hotmail.com>
2018-12-11 10:04:29 +0100

    CameraNode

--- Game/src/GameFacade/CameraNode.cpp
+++ Game/src/GameFacade/CameraNode.cpp
@@ -1,1 +1,12 @@
-/home/gonzalo/Escritorio/CameraNode.cpp
+#include "CameraNode.h"
+
+void CameraNode::update(float x, float y, float z, float rx, float ry, float rz)
+{
+    node->setPosition(vector3d<f32>(x, y, z));
+    node->setTarget(vector3d<f32>(rx, ry, rz));
+}
+
+void CameraNode::setFOV(float fov)
+{
+    node->setFOV(fov);
+}
--- Game/src/GameFacade/CameraNode.h
+++ Game/src/GameFacade/CameraNode.h
@@ -1,1 +1,23 @@
-/home/gonzalo/Escritorio/CameraNode.h
+#pragma once
+#include "RenderIrrlicht.h"
+
+class RenderIrrlicht;
+class CameraNode{
+    private:
+        ICameraSceneNode* node;
+    public:
+        CameraNode()
+        {
+            node = RenderIrrlicht::getInstance()->getSMgr()->addCameraSceneNode(0, vector3df(0,0,0), vector3df(0,0,0));
+            
+        }
+
+        ~CameraNode()
+        {
+            delete node;
+        }
+
+        void update(float x, float y, float z, float rx, float ry, float rz);
+        void setFOV(float fov);
+
+};
