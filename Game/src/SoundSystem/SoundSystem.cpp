3b421e2be8cf547dce43261dbc99d05e74aeaddb
Gonzalo.Manjaro <fxgxcx@hotmail.com>
2018-12-19 19:09:53 +0100

    Fmod Compila

--- Game/src/SoundSystem/SoundSystem.cpp
+++ Game/src/SoundSystem/SoundSystem.cpp
@@ -1,1 +1,39 @@
 #include "SoundSystem.h"
+
+
+SoundSystem* SoundSystem::only_instance = nullptr;
+
+SoundSystem::SoundSystem()
+{
+    
+}
+
+
+
+void SoundSystem::init(const char* filename)
+{
+    FMOD_STUDIO_SYSTEM* system = nullptr;
+
+    FMOD_Studio_System_Initialize(system, 
+                                    2,
+                                    FMOD_STUDIO_INIT_NORMAL,
+                                    FMOD_INIT_NORMAL,
+                                    0);
+    /*
+    result = fmod->createSound(this->filename.c_str(), FMOD_DEFAULT, 0, &(this->sound));
+	if (result != FMOD_OK) {
+		return false;
+	}
+	return true;
+    */
+}
+
+void SoundSystem::play(float volume)
+{
+
+}
+
+void SoundSystem::pause()
+{
+
+}
--- Game/src/SoundSystem/SoundSystem.h
+++ Game/src/SoundSystem/SoundSystem.h
@@ -1,1 +1,23 @@
 #include <fmod_studio.h>
+
+
+class SoundSystem 
+{
+    private:
+        static SoundSystem* only_instance;
+        SoundSystem();
+    public:
+        static SoundSystem* getInstance()
+        {
+            if(!only_instance) only_instance = new SoundSystem();
+
+            return only_instance;
+        }
+
+        ~SoundSystem(){only_instance = nullptr;}
+
+
+        void init(const char* filename);
+        void play(float volume);
+        void pause();
+};
