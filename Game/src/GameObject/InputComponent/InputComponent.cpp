fd5616c07de3ec81e8a30e1804806d258bd8bdf5
Gonzalo.Manjaro <fxgxcx@hotmail.com>
2019-01-05 15:11:36 +0100

    Reajuste

--- Game/src/GameObject/InputComponent/InputComponent.cpp
+++ Game/src/GameObject/InputComponent/InputComponent.cpp
@@ -1,12 +1,8 @@
 #include "InputComponent.h"
-//#include "InputFacade.h"
 #include "ShootComponent.h"
-//#include <irrlicht.h>
 
 void InputComponent::update(float cursorX, float cursorY)
 {
-    //InputFacade* interface = InputFacade::getInstance();
-
     int enX = 0, enY = 0;
 
 
@@ -32,33 +28,4 @@ void InputComponent::update(float cursorX, float cursorY)
     if(gameObject->getComponent<BPhysicComponent>()!=nullptr){
         gameObject->getComponent<BPhysicComponent>()->moveObject(enX ,enY, 0.f, cursorX, cursorY);
     }
-
-
-    /*
-    if(interface->IsKeyDown((irr::EKEY_CODE)w_)){
-        enY+=1;
-    }    
-    if(interface->IsKeyDown((irr::EKEY_CODE)a_)){
-        enX-=1;
-    }
-    if(interface->IsKeyDown((irr::EKEY_CODE)s_)){
-        enY-=1;
-    }
-    if(interface->IsKeyDown((irr::EKEY_CODE)d_)){
-        enX+=1;
-    }
-    if(interface->IsKeyDown((irr::EKEY_CODE)d_)){
-        enX+=1;
-    }
-    
-
-    if(interface->IsKeyDown((irr::EKEY_CODE)space_)){
-        if(gameObject->getComponent<ShootComponent>()!=nullptr)
-        gameObject->getComponent<ShootComponent>()->shoot();
-    }
-
-    if(gameObject->getComponent<BPhysicComponent>()!=nullptr){
-        gameObject->getComponent<BPhysicComponent>()->moveObject(enX ,enY, 0.f, cursorX, cursorY);
-    }
-    */
 }
