b1b9631b9ac25744ed98245fc1ab4904f6374f1f
Derdinald <dcarcamo@outlook.es>
2019-02-01 14:50:31 +0100

    Arreglar make motor sonido

--- Game/src/SoundSystem/SoundSystem.h
+++ Game/src/SoundSystem/SoundSystem.h
@@ -50,21 +50,21 @@ public:
 
     void LoadBank(const string& strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
     void LoadEvent(const string& strEventName);
-    void Loadsound(const string& strSoundName, bool b3d = true, bool bLooping = false, bool bStream = false);
+    void LoadSound(const string& strSoundName, bool b3d = true, bool bLooping = false, bool bStream = false);
     void UnLoadSound(const string& strSoundName);
     void Set3dListenerAndOrientation(const Vector3& vPos = Vector3{ 0, 0, 0 }, float fVolumedB = 0.0f);
-    void PlaySound(const string& strSoundName, const Vector3& vPos = Vector3{ 0, 0, 0 }, float fVolumedB = 0.0f);
+    int PlaySounds(const string& strSoundName, const Vector3& vPos = Vector3{ 0, 0, 0 }, float fVolumedB = 0.0f);
     void PlayEvent(const string& strEventName);
     void StopChannel(int nChannelId);
     void StopEvent(const string& strEventName, bool bImmediate = false);
-    void GeteventParameter(const string& strEventName, const string% strEventParameter, float* parameter);
-    void SetEventParameter(const string& strEventName, const string& strParameterName, flaot fValue);
+    void GetEventParameter(const string& strEventName, const string& strEventParameter, float* parameter);
+    void SetEventParameter(const string& strEventName, const string& strParameterName, float fValue);
     void StopAllChannels();
     void SetChannel3dPosition(int nChannelId, const Vector3& vPosition);
-    void SetChannelvolume(int nChannelId, float fVolumedB);
+    void SetChannelVolume(int nChannelId, float fVolumedB);
     bool IsPlaying(int nChannelId) const;
     bool IsEventPlaying(const string& strEventName) const;
     float dbToVolume(float db);
-    float VolumeTodb(float volume);
-    FMOD_VECTOR VectorToFmod(const Vector& vPosition);
+    float VolumeTodB(float volume);
+    FMOD_VECTOR VectorToFmod(const Vector3& vPosition);
 };
