8c4985e37b282b7c04ee27a6c0c715935db0c87e
Derdinald <dcarcamo@outlook.es>
2019-02-26 20:43:13 +0100

    Enemigos se quedan a cierta distancia de ti

--- Game/src/GameObject/IAComponent/IASeguimiento.cpp
+++ Game/src/GameObject/IAComponent/IASeguimiento.cpp
@@ -7,16 +7,24 @@
 
 bool IA_Seg_ToPlayer::run()
 {
+      float d = round(sqrt(pow(main->getX() - owner->getX(),2) + pow(main->getY() - owner->getY(),2)));
+     
     
     float rZ = atan2(owner->getY() - main->getY(), owner->getX() - main->getX());
     rZ += PI/2.0;
     rZ = rZ * 180/PI;
     if (rZ < 0)
         rZ += 360;
-    owner->getComponent<BPhysicComponent>()->setvMax(3.f);
+    if(d< 4.f) {
+        owner->getComponent<BPhysicComponent>()->setvMax(0.f);
+
+    }else{
+        owner->getComponent<BPhysicComponent>()->setvMax(3.f);
+    }
 
     owner->getComponent<RenderComponent>()->setTexture((char*)"res/yelow.bmp");
 
     owner->getComponent<BPhysicComponent>()->setVelocity(rZ, true);
     return true;
 }
+
