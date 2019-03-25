c637e536940dae1aed6f9fe60b83ecebbf17e612
Derdinald <dcarcamo@outlook.es>
2019-03-25 21:14:19 +0100

    Motor sonido integrado

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -1,6 +1,12 @@
 #include "Game.h"
 #include "LevelLoader.h"
+#include "SpecificSoundEvent.h"
+#include <SFML/Window.hpp>
 int main(){
+
+
+
+    
     LevelLoader::getInstance()->writeFile();
     Game::getInstance()->run();
 
