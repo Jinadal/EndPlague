950b2c421b323f50ebee802cd938a84d63f1e424
Gonzalo.Manjaro <fxgxcx@hotmail.com>
2019-03-27 12:06:21 +0100

    Borrado de couts

--- Game/src/GameState/EndState.cpp
+++ Game/src/GameState/EndState.cpp
@@ -21,11 +21,9 @@ void EndState::initState(){
 
     if(ScoreManager::getInstance()->getWin())
     {
-        std::cout<<"HasGanado\n";
         Render::getInstance()->getMenu()->setMenuBackground((char*)"res/sprites/Win.png");
     }else
     {
-        std::cout<<"Has Perdido\n";
         Render::getInstance()->getMenu()->setMenuBackground((char*)"res/sprites/Loose.png");
     }
     std::cout<<"Puntuacion final: "<<HUD::getInstance()->getScore()<<"\n";
