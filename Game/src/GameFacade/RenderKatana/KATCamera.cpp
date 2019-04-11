6be3162ec1fa0531a8bb9dac244bdd2abaa1a420
Gonzalo.Manjaro <fxgxcx@hotmail.com>
2019-04-12 00:23:31 +0200

    Funcional, listo para cerrar rama

--- Game/src/GameFacade/RenderKatana/KATCamera.cpp
+++ Game/src/GameFacade/RenderKatana/KATCamera.cpp
@@ -16,23 +16,17 @@ void KATCamera::update(float x, float y, float z, float rx, float ry, float rz)
 {
     TTransform* translate = (TTransform*)camera->getFather()->getEntity();
     TTransform* rotate = (TTransform*)camera->getFather()->getFather()->getFather()->getEntity();
-    //TTransform* scale = (TTransform*)camera->getFather()->getFather()->getEntity();
-
 
     translate->identity();
-    //scale->identity();
     rotate->identity();
 
-    rotate->rotate(1, 0, 0, 3.14);
-    //rotate->rotate(0, 1, 0, 3.14);
+    float dz = z - rz;
+    float dy = y - ry;
 
-    //scale->scale(-1,-1,1);
+    rotate->rotate(0, 0, 1, 3.1416);
+    rotate->rotate(1, 0, 0, 3.1416);
+    rotate->rotate(1, 0, 0, atan(dy/dz));
     translate->translate(-x, y, z);
-   
-    
-
-
-   //KATRender::getInstance()->setCamera(-x, y, z, -rx, ry, rz);
 }
 
 void KATCamera::setFOV(float fov)
