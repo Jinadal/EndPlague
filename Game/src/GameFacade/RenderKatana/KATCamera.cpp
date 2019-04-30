8961f6fba2b870bc58be361082e93ab4d48d16ff
Derdinald <dcarcamo@outlook.es>
2019-04-30 16:32:23 +0200

    Camara ortogonal version 1,5 (Tiene que pinta que no es lo que Nacho quiere)

--- Game/src/GameFacade/RenderKatana/KATCamera.cpp
+++ Game/src/GameFacade/RenderKatana/KATCamera.cpp
@@ -29,7 +29,6 @@ void KATCamera::update(float x, float y, float z, float rx, float ry, float rz, 
     }else{
         rotate->rotate(0, 0, 1, 3.1416);
         rotate->rotate(1, 0, 0, 3.1416);
-        //rotate->rotate(1, 0, 0, atan(dy/dz));
         float ajust = ry - y;
         y = y +ajust;
     }
